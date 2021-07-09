c2c8ed25f3843c8df30a751c4fd1148b9f8f9877
Jakub Fijałkowski <fijalkowski.jakub@icloud.com>
2021-07-09 16:25:47 +0200

    fix errors \

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import React from 'react'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import pic01_1 from '../images/pic01_1'
 
 class Main extends React.Component {
   render() {
